37c7450f13a1674f112efe1973821fb75ff6a675
Silas P. Ladislau <splhead@gmail.com>
2020-03-31 10:20:06 -0400

    Frontend do app

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
 import { Container } from './styles';
 import colors from '../../styles/colors';
 import logo from '../../assets/logo.svg';
 
+import api from '../../services/api';
+
 export default function NewIncident() {
+  const ongId = localStorage.getItem('ongId');
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+
+  const history = useHistory();
+
+  async function handleNewIncident(e) {
+    e.preventDefault();
+    const data = {
+      title,
+      description,
+      value,
+    };
+
+    try {
+      await api.post('incidents', data, {
+        headers: {
+          Authorization: ongId,
+        },
+      });
+      history.push('/profile');
+    } catch (error) {
+      alert('Erro ao cadastrar, tente novamente.');
+    }
+  }
+
   return (
     <Container>
       <div className="content">
@@ -24,10 +53,22 @@ export default function NewIncident() {
             Voltar para home
           </Link>
         </section>
-        <form>
-          <input placeholder="Título do caso" />
-          <textarea placeholder="Descrição" />
-          <input placeholder="whatsapp" />
+        <form onSubmit={handleNewIncident}>
+          <input
+            placeholder="Título do caso"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+          <textarea
+            placeholder="Descrição"
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+          />
+          <input
+            placeholder="whatsapp"
+            value={value}
+            onChange={e => setValue(e.target.value)}
+          />
 
           <input placeholder="Valor em reais" />
 
--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiLogIn } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import { Container } from './styles';
 import logo from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 import colors from '../../styles/colors';
 
+import api from '../../services/api';
+
 export default function SignIn() {
+  const [id, setId] = useState('');
+
+  const history = useHistory();
+
+  async function handleSignIn(e) {
+    e.preventDefault();
+
+    try {
+      const response = await api.post('sessions', { id });
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+      history.push('/profile');
+    } catch (error) {
+      alert('Falha no login, tente novamente.');
+    }
+  }
+
   return (
     <Container>
       <section className="form">
         <img src={logo} alt="BeaHero" />
-        <form>
+        <form onSubmit={handleSignIn}>
           <h1>Faça seu logon</h1>
-          <input placeholder="sua ID" />
+          <input
+            placeholder="sua ID"
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
           <button className="button" type="submit">
             Entrar
           </button>
